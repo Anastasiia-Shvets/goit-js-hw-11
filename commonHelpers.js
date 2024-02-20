99d95eac62757f10c3f8d9dcc51c8fb379f7beb3
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2024-02-20 12:00:58 +0000

    Deploying to gh-pages from @ Anastasiia-Shvets/goit-js-hw-11@6fe20df415cfc05318a90da5b69adfb16bfd9ac7 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,5 +1,5 @@
-import{i as a}from"./assets/vendor-ad859c2f.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();function m(o){const s="42280765-41e7252ac679e023dc9db9847",e=o,n="https://pixabay.com",t="/api/",r=`?key=${s}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`,l=n+t+r;return fetch(l).then(i=>{if(i.ok)return i.json();throw new Error(i.status)}).then(i=>i.hits.map(c=>({id:c.id,imageUrl:c.largeImageURL}))).catch(i=>{console.error("There has been a problem with your fetch operation:",i)})}const u={formElem:document.querySelector(".form"),inptElem:document.querySelector(".text-form"),imagesElem:document.querySelector(".gallery")};function h(o){const s=o.map(e=>`<div class="img-card">
-    <li class="gallery-item">
+import{i as c}from"./assets/vendor-ad859c2f.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();function u(o){const s="42280765-41e7252ac679e023dc9db9847",e=o,a="https://pixabay.com",t="/api/",r=`?key=${s}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`,l=a+t+r;return fetch(l).then(n=>{if(n.ok)return n.json();throw new Error(n.status)}).then(n=>n.hits.map(i=>({id:i.id,imageUrl:i.largeImageURL,webformatURL:i.webformatURL,tags:i.alt,likes:i.likes,views:i.views,comments:i.comments,downloads:i.downloads}))).catch(n=>{console.error("There has been a problem with your fetch operation:",n)})}const m={formElem:document.querySelector(".form"),inptElem:document.querySelector(".text-form"),imagesElem:document.querySelector(".gallery-cards")};function f(o){const s=o.map(e=>(console.log(o),`<li class="gallery-item">
+    <div class="img-card">
     <a class="gallery-link" href="${e.largeImageURL}">
     <img
         class="gallery-image"
@@ -8,13 +8,12 @@ import{i as a}from"./assets/vendor-ad859c2f.js";(function(){const s=document.cre
         width=360
     />
     </a>
-    </div>
-    <div class="img-body">
-    <h3 class="subtitle">'${e.likes}'<h3 class="subtitle">
-    <h3 class="subtitle">'${e.views}'<h3 class="subtitle">
-    <h3 class="subtitle">'${e.comments}'<h3 class="subtitle">
-    <h3 class="subtitle">'${e.downloads}'<h3 class="subtitle">
+    <p class="item-text">Likes: ${e.likes}</p>
+    <p class="item-text">Views: ${e.views}</p>
+    <p class="item-text">Comments: ${e.comments}</p>
+    <p class="item-text">Downloads: ${e.downloads}</p>
     </div>
     </a>
-    </li>`).join();u.imagesElem.innerHTML=s}u.formElem.addEventListener("submit",o=>{o.preventDefault();const s=o.target.elements.query.value.trim();if(s===""){a.error({title:"Sorry",message:"Please enter text to search for.",position:"topRight"});return}m(s).then(e=>{e.length===0?a.warning({title:"Sorry",message:"There are no images matching your search query. Please try again!",position:"topRight"}):(h(e),console.log(e))}).catch(e=>{a.error({title:"Error",message:"An error occurred while fetching images. Please try again later.",position:"topRight"}),console.log(e)}),o.target.reset()});
+    </li>
+    </div>`)).join("");m.imagesElem.innerHTML=s}m.formElem.addEventListener("submit",o=>{o.preventDefault();const s=o.target.elements.query.value.trim();if(s===""){c.error({title:"Sorry",message:"Please enter text to search for.",position:"topRight"});return}u(s).then(e=>{e.length===0?c.warning({title:"Sorry",message:"There are no images matching your search query. Please try again!",position:"topRight"}):(f(e),console.log(e))}).catch(e=>{c.error({title:"Error",message:"An error occurred while fetching images. Please try again later.",position:"topRight"}),console.log(e)}),o.target.reset()});
 //# sourceMappingURL=commonHelpers.js.map
